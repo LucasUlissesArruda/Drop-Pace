288aefdad7ea77924bbfa6067cf52f71bd9f12c9
Lucas Arruda <168601194+LucasUlissesArruda@users.noreply.github.com>
2024-12-04 16:49:04 -0300

    Add files via upload

--- drop-pace.sql
+++ drop-pace.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04/12/2024 às 20:47
+-- Tempo de geração: 04/12/2024 às 20:48
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
